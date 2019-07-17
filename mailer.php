0e88313cfd3b217860c0d61761d30be316235473
unknown <ayushpanara@gmail.com>
2019-07-17 23:19:04 +0530

    new update in subscribe 2

--- mailer.php
+++ mailer.php
@@ -4,11 +4,11 @@ $email_id  = $REQUEST['email'];
 $msg = $email_id;
 
 // use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
+$msg = wordwrap($msg,700);
 
 // send email
 if(mail("help.betamurungai@gmail.com","New Subscription mail ",$msg))
 	header( 'Location: /index.html' ) ;
 else
-	header( 'Location: /index.html' ) ;
+	echo "Arre";;
 ?>
