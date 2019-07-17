edf7ab383dfa842caba1f9f27fb4adeec330ffda
unknown <ayushpanara@gmail.com>
2019-07-17 23:33:41 +0530

    new update in subscribe changed request parameter

--- mailer.php
+++ mailer.php
@@ -1,13 +1,13 @@
 <?php
 // the message
-$email_id  = $REQUEST['email'];
+$email_id  = $_REQUEST['email'];
 $msg = $email_id;
 
 // use wordwrap() if lines are longer than 70 characters
 $msg = wordwrap($msg,700);
 
 // send email
-if(mail("ayushmpanara@gmail.com","New Subscription mail ",$msg))
+if(mail("help.betamurungai@gmail.com","New Subscription mail ",$msg))
 	header( 'Location: /index.html' ) ;
 else
 	echo "Arre";
